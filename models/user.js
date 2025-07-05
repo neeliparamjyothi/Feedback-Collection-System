8ead0952ea2ba99b7b549fcc965410b5498ba669
Param Jyothi <152426396+neeliparamjyothi@users.noreply.github.com>
2025-07-05 14:49:23 +0530

    Updated

--- models/user.js
+++ models/user.js
@@ -2,6 +2,11 @@ const mongoose=require("mongoose");
 const passportLocalMongoose=require("passport-local-mongoose");
 
 const customerSchema=new mongoose.Schema({
+    username: {     
+      type: String,
+      required: true,
+      unique: true
+     },
     email:{
         type:String,
         required:true
